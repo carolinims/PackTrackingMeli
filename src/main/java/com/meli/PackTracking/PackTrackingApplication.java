f2fed09fbe49c56f3885ed2d27b61c1b388d2963
Carolini Moraes da Silva <carolinimo@hotmail.com>
2025-03-22 13:56:23 -0300

    solve .ignore coflict

---
+++ src/main/java/com/meli/PackTracking/PackTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.meli.PackTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackTrackingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/PackTracking/PackTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.PackTracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
